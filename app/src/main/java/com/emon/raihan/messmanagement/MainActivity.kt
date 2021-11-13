b02fb05d3db82a7d95b2324687f0970a6592d8f3
emonrait <emonrait@gmail.com>
2021-11-13 19:54:37 +0600

    Project Upload

---
+++ app/src/main/java/com/emon/raihan/messmanagement/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.emon.raihan.messmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
